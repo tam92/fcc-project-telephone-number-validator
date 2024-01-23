315678018f19d22b39e6367434665a3956cf6e4e
Tamara <29234416+tam92@users.noreply.github.com>
2024-01-23 13:40:44 +0100

    Add US phone number check functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+const phoneNumberRegex = /^[1]?[\s]?(?:[(]+[\d]{3}[)]+|[\d]{3})[\s]?[-\s]?[\d]{3}[-\s]?[\d]{4}$/;
+
+const checkNumber = () => {
+    inputIsEmpty();
+
+    phoneNumberRegex.test(userInput.value) 
+        ? resultsDiv.textContent = "Valid US number: " + userInput.value
+        : resultsDiv.textContent = "Invalid US number: " + userInput.value;
+};
+
+const inputIsEmpty = () => {
+    if (!userInput.value) {
+        alert("Please provide a phone number");
+    }
+};
+
+const cleanInput = () => {
+    resultsDiv.textContent = "";
+}
+
+checkBtn.addEventListener("click", checkNumber);
+clearBtn.addEventListener("click", cleanInput);
